bb7f58b8b1c2e136bec143d7f00055aa5d66892d
James Muir <JRMuir09@Gmail.com>
2016-01-22 19:35:42 +0000

    Changes to be committed: 	new file:   cookieTest/index.html 	modified:   testFour/js/main.js 	Added code to start physics

--- testFour/js/main.js
+++ testFour/js/main.js
@@ -1,11 +1,22 @@
-var Main = function(game){
+var Main = function(game) {
 
 };
 
 Main.prototype = {
 
 	create: function() {
-        this.game.stage.backgroundColor = "#333333"
+        // Sets background color
+        this.game.stage.backgroundColor = "#333333";
+        
+        // Sets up using the p2 physics engine
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
+        
+        // Sets the gravity 
+        this.game.physics.p2.gravity.y = 1000;
+        
+        // Creates collision groups 
+        // TODO: Figure out what and how exactly this works
+        this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
 	},
 
 	update: function() {
